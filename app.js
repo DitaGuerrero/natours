35e7588652c2512dd32a4a4cd98bcf1773cbb1f9
DitaGuerrero <dita66uerrero@gmail.com>
2022-08-24 17:40:06 -0400

    Feat Implemented globalErrorHandler * Create new errors using AppError.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./util/AppError');
+const globlalErrorHandler = require('./error/errorController');
 
 const app = express();
 //  The order middleware appears will be the order they apply to routes
@@ -16,10 +18,10 @@ const app = express();
 //Parse to object the request and response
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -30,5 +32,25 @@ app.use(express.static(`${__dirname}/public`));
 //These routers are middleware as well, so we can use app.use
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Route for unhandled routes
+app.all('*', (req, res, next) => {
+  // First Approach
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  //
+  // // The only arg that next func receives is an error
+  // // So it will escape any other middleware and will bo straight
+  // // to the global error handling middleware
+  // next(err);
+
+  //  Using AppError
+  const err = new AppError(`Can't find ${req.originalUrl} on this server`, 404);
+  next(err);
+});
+
+// Error handling Built-in
+app.use(globlalErrorHandler);
 
 module.exports = app;
